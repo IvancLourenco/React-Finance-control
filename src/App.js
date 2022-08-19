e573f85a489471a6bbcc84287243913ff20d07ed
unknown <ivancampos039@gmail.com>
2022-08-18 21:19:05 -0300

    React projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GlobalStyle from "./style/global";
+const App = () => {
+    return (
+        <>
+            <div>App</div>
+            <GlobalStyle/>
+        </>
+    );
+};
+
+export default App;
